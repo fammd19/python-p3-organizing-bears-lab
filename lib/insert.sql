119e8a9a5eaf0a7c5b0d187ec9c0dce4c06703c5
Fiona Mellink <fimellink@gmail.com>
2024-04-21 11:39:49 +1000

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex, alive) VALUES ("Mr. Chocolate", "M", FALSE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Rowdy", 5, "M", TRUE);
+INSERT INTO bears (name, sex, alive) VALUES ("Tabitha", "F", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Sergeant Brown", 24, "M", TRUE);
+INSERT INTO bears (name, age, sex, alive) VALUES ("Melissa", 8, "F", FALSE);
+INSERT INTO bears (name, sex, alive) VALUES ("Grinch", "M", TRUE);
+INSERT INTO bears (name, sex, alive) VALUES ("Wendy", "F", TRUE);
+INSERT INTO bears (name, sex, alive) VALUES (NULL, "M", FALSE)
+
